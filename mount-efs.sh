74978dad8f207c2db90db242162d891d1645d304
Pablo del Pino <pdelpino@delpinoconsulting.com>
2018-03-12 15:00:32 -0500

    Update mount-efs.sh

--- mount-efs.sh
+++ mount-efs.sh
@@ -6,13 +6,15 @@ set -e
 # Error checking
 : ${REGION:?"[ERROR] REGION must be set."}
 : ${VOLUME_NAME:?"[ERROR] VOLUME_NAME must be set."}
+: ${MOUNT_POINT:?"[ERROR] MOUNT_POINT must be set."}
 
 VOLUME_NAME: Replace here with Volume endpoint assigned by AWS (ex: fs-fxxxxxx)
-REGION: replace here with intended deployment region (ex: us-east-1, us-west-2)
+REGION: Replace here with intended deployment region (ex: us-east-1, us-west-2)
+MOUNT_POINT: Replace here with chosen mount point (ex: /mnt/efs)
 
 #Create folder and mount EFS volume
-mkdir -p /mnt/efs
-mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2 ${VOLUME_NAME}.efs.${REGION}.amazonaws.com:/ /mnt/efs
+mkdir -p ${MOUNT_POINT}
+mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2 ${VOLUME_NAME}.efs.${REGION}.amazonaws.com:/ ${MOUNT_POINT}
 
 #Automount EFS Volume
 fstab=/etc/fstab
